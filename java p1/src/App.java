ce7c7d057304306b2ac46c818a5a75ca160801d1
RamadanDY <daudalaramadan@gmail.com>
2023-11-12 00:44:13 +0000

    first commit

---
+++ java p1/src/App.java
@@ -0,0 +1,91 @@
+/*public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
+*/
+
+
+
+/////try work
+
+// import java.util.Arrays;
+
+// public class App {
+//     public static void main(String[] args) {
+//         int[] numbers ={1,3,4};
+//         System.out.println(Arrays.toString(numbers));
+//     }
+// }
+
+// int[] numbers = new int[5];
+        // numbers[0] = 1;
+        // numbers[1] = 2;
+        // numbers[2] = 3;
+        // numbers[3] = 4;
+        // numbers[4] = 5;
+        
+        // System.out.println(Arrays.toString(numbers));
+
+        // int[] numbers = new int[5];
+        // for(int i=0; i < numbers.length; i++){
+        //     numbers[i] = i + 1;
+        //     System.out.println(Arrays.toString(numbers));
+        // }
+
+        /////writing a code to print 200 only or a one dimensional array
+        // int [] App = { 10,20,30,40,50,48};
+        // for(int i =0;i<App.length;i++){
+        // System.out.println((App[4]));
+        // }
+
+        // /////writing a one dimensional array
+        // int[] Kos = {10,30,40,60};
+        // for(int i =0;i<Kos.length;i++){
+        //     System.out.println((Kos[0]));
+        // }
+
+        ////lets moove to some two dimensional arrays 
+
+// import java.util.Arrays;
+
+
+// public class App {
+//     public static void main (String[] args){
+        
+//         // int  Ram [] []  ={{1,2,3,},{4,5,6}};
+//         // for(int i = 0;i<2;i++){
+//         //     for(int j = 0;j<3;j++){
+//         //     System.out.print(Ram[i][j]+"");
+//         // }
+//         // System.out.println();}
+
+//     }
+// }
+
+// public class App {
+//     public static void main(String[] args){
+//         int [] [] Ram = {{1,2,3},{4,5,6}};
+// for(int i = 0;i<2;i++){
+//     for(int j = 0;j<3;j++){
+//         System.out.print(Ram[i][j]);
+//     }
+//     System.out.println();
+   
+// }
+// }
+// }
+
+
+//////recup on array for the night 
+
+public class App{
+    public static void main(String[] args){
+        int [] Got = {1,2,3};
+        for(int i = 0;i<Got.length;i++){
+            System.out.println(Got[0]);
+        
+        }
+
+    } 
+}
